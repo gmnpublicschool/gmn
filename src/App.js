cf366271a7038c05a6ed08e80033c4214f4dffce
unknown <kundan.kumar@mantralabsglobal.com>
2024-05-25 13:30:18 +0530

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React learning
         </a>
       </header>
     </div>
